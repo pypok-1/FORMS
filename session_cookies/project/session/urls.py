6a8ea1a3d8f12a343a3d854627a5b7dfbf94d379
Eva <evacherkashyna9@gmail.com>
2025-12-29 00:27:41 +0200

    Update urls.py

--- session_cookies/project/session/urls.py
+++ session_cookies/project/session/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # cookies
     path("set-cookies/", views.set_cookies, name="set_cookies"),
     path("delete-cookies/", views.delete_cookies, name="delete_cookies"),
     path("get-cookies/", views.get_cookies, name="get_cookies"),
@@ -14,3 +13,4 @@ urlpatterns = [
     path('reset_visit_counter/', views.reset_visit_counter, name='reset_visit_counter'),
     path('visit_counter/', views.visit_counter, name='visit_counter'),
 ]
+
