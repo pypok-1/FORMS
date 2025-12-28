5dc92cb47194157d129ad5cc657a30efa0e4008c
Eva <evacherkashyna9@gmail.com>
2025-12-28 23:59:53 +0200

    Update urls.py

--- session_cookies/project/accounts/urls.py
+++ session_cookies/project/accounts/urls.py
@@ -31,4 +31,18 @@ urlpatterns = [
         ),
         name="password_reset_complete",
     ),
+    path(
+     "password_change/",
+     auth_views.PasswordChangeView.as_view(
+         template_name="accounts/registration/password_change.html"
+     ),
+     name="password_change",
+    ),
+    path(
+     "password_change/done/",
+     auth_views.PasswordChangeDoneView.as_view(
+         template_name="accounts/registration/password_change_done.html"
+     ),
+     name="password_change_done",
+    ),
 ]
